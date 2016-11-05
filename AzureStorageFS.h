8e4e3d92f116c3835fc62882521eaae2fdbce78d
andyliuliming <andyliuliming@outlook.com>
2016-11-05 15:40:27 +0800

    block device insead of FS.

--- AzureStorageFS.h
+++ AzureStorageFS.h
@@ -21,43 +21,49 @@
 class AzureStorageFS
 {
   private:
-	static const char *_root;
-    static void AbsPath(char dest[PATH_MAX], const char *path);
-
+	
   public:
     static AzureStorageFSEnv *asEnv;
     static AzureStorageAdapter *asAdapter;
 
-    static void set_rootdir(const char *path);
-    static int wrap_getattr(const char *path, struct stat *statbuf);
-    static int wrap_readlink(const char *path, char *link, size_t size);
-    static int wrap_mknod(const char *path, mode_t mode, dev_t dev);
-    static int wrap_mkdir(const char *path, mode_t mode);
-    static int wrap_unlink(const char *path);
-    static int wrap_rmdir(const char *path);
-    static int wrap_symlink(const char *path, const char *link);
-    static int wrap_rename(const char *path, const char *newpath);
-    static int wrap_link(const char *path, const char *newpath);
-    static int wrap_chmod(const char *path, mode_t mode);
-    static int wrap_chown(const char *path, uid_t uid, gid_t gid);
-    static int wrap_truncate(const char *path, off_t newSize);
-    static int wrap_utime(const char *path, struct utimbuf *ubuf);
-    static int wrap_open(const char *path, struct fuse_file_info *fileInfo);
-    static int wrap_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo);
-    static int wrap_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fileInfo);
-    static int wrap_statfs(const char *path, struct statvfs *statInfo);
-    static int wrap_flush(const char *path, struct fuse_file_info *fileInfo);
-    static int wrap_release(const char *path, struct fuse_file_info *fileInfo);
-    static int wrap_fsync(const char *path, int datasync, struct fuse_file_info *fi);
-    static int wrap_setxattr(const char *path, const char *name, const char *value, size_t size, int flags);
-    static int wrap_getxattr(const char *path, const char *name, char *value, size_t size);
-    static int wrap_listxattr(const char *path, char *list, size_t size);
-    static int wrap_removexattr(const char *path, const char *name);
-    static int wrap_opendir(const char *path, struct fuse_file_info *fileInfo);
-    static int wrap_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fileInfo);
-    static int wrap_releasedir(const char *path, struct fuse_file_info *fileInfo);
-    static int wrap_fsyncdir(const char *path, int datasync, struct fuse_file_info *fileInfo);
-    static void *wrap_init(struct fuse_conn_info *conn);
+    static void *data;
+    static int xmpl_debug;
+
+static int xmp_read(void *buf, u_int32_t len, u_int64_t offset, void *userdata)
+{
+  if (*(int *)userdata)
+    fprintf(stderr, "R - %lu, %u\n", offset, len);
+  memcpy(buf, (char *)data + offset, len);
+  return 0;
+}
+
+static int xmp_write(const void *buf, u_int32_t len, u_int64_t offset, void *userdata)
+{
+  if (*(int *)userdata)
+    fprintf(stderr, "W - %lu, %u\n", offset, len);
+  memcpy((char *)data + offset, buf, len);
+  return 0;
+}
+
+static void xmp_disc(void *userdata)
+{
+  (void)(userdata);
+  fprintf(stderr, "Received a disconnect request.\n");
+}
+
+static int xmp_flush(void *userdata)
+{
+  (void)(userdata);
+  fprintf(stderr, "Received a flush request.\n");
+  return 0;
+}
+
+static int xmp_trim(u_int64_t from, u_int32_t len, void *userdata)
+{
+  (void)(userdata);
+  fprintf(stderr, "T - %lu, %u\n", from, len);
+  return 0;
+}
 };
 
 #endif //azure_storage_fs_hh
--- busestorage.cpp
+++ busestorage.cpp
@@ -5,52 +5,15 @@
 #include "AzureStorageFS.h"
 #include "buse.h"
 
-static void *data;
-static int xmpl_debug = 1;
 
-static int xmp_read(void *buf, u_int32_t len, u_int64_t offset, void *userdata)
-{
-  if (*(int *)userdata)
-    fprintf(stderr, "R - %lu, %u\n", offset, len);
-  memcpy(buf, (char *)data + offset, len);
-  return 0;
-}
-
-static int xmp_write(const void *buf, u_int32_t len, u_int64_t offset, void *userdata)
-{
-  if (*(int *)userdata)
-    fprintf(stderr, "W - %lu, %u\n", offset, len);
-  memcpy((char *)data + offset, buf, len);
-  return 0;
-}
-
-static void xmp_disc(void *userdata)
-{
-  (void)(userdata);
-  fprintf(stderr, "Received a disconnect request.\n");
-}
-
-static int xmp_flush(void *userdata)
-{
-  (void)(userdata);
-  fprintf(stderr, "Received a flush request.\n");
-  return 0;
-}
-
-static int xmp_trim(u_int64_t from, u_int32_t len, void *userdata)
-{
-  (void)(userdata);
-  fprintf(stderr, "T - %lu, %u\n", from, len);
-  return 0;
-}
 
 
 static struct buse_operations aop = {
-  .read = xmp_read,
-  .write = xmp_write,
-  .disc = xmp_disc,
-  .flush = xmp_flush,
-  .trim = xmp_trim,
+  .read = AzureStorageFS::xmp_read,
+  .write = AzureStorageFS::xmp_write,
+  .disc = AzureStorageFS::xmp_disc,
+  .flush = AzureStorageFS::xmp_flush,
+  .trim = AzureStorageFS::xmp_trim,
   .size = 128 * 1024 * 1024,
 };
 
@@ -75,7 +38,7 @@ int main(int argc, char *argv[])
   openlog("fusestorage", LOG_CONS | LOG_PID, LOG_USER);
 
 
-  data = malloc(aop.size);
+  AzureStorageFS::data = malloc(aop.size);
 
-  return buse_main(argv[1], &aop, (void *)&xmpl_debug);
+  return buse_main(argv[1], &aop, (void *)&AzureStorageFS::xmpl_debug);
 }
